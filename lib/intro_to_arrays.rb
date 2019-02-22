79567e4c956ef4849b4c465db1f52f89edac7b23
github username <github email address>
2019-02-22 01:51:07 -0700

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,48 @@
+def instantiate_new_array
+    array = []
+end
+
+
+
+def array_with_two_elements
+    two_element_array = ["Sarah", "Daniels"]
+end
+
+
+
+def first_element(array)
+    my_first_element = ["Welcome to New York", "hi", "how do you do"]
+    array[0]
+end
+
+
+
+def third_element(array)
+    my_third_element = ["hi", "hello", "how do you do"]
+    array[2]
+end
+
+
+def last_element(array)
+    my_last_element = ["hi", "hello", "how do you do"]
+array[-1]
+end
+
+
+
+def first_element_with_array_methods(array)
+    first_country = ["Thailand", "germany", "usa"]
+    first_country.first
+end
+
+
+def last_element_with_array_methods(array)
+    last_country = ["Thailand", "germany", "Myanmar"]
+    last_country.last
+end
+
+
+def length_of_array(array)
+    length_of_array = [1, 2, 3, 4, 5, 6, 7, 8]
+    length_of_array.length
+    end
